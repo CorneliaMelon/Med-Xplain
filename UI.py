96cce4d6449d4a26cd34e512878b7d6cb16447f7
Julian Wyatt <julian_wyatt@hotmail.com>
2023-10-21 17:07:08 +0100

    Sped up initial search query by not using LLM Integrated LLM for initial response

--- UI.py
+++ UI.py
@@ -1,31 +1,42 @@
 import streamlit as st
 
-from chatbot_main import chatbot_response
+from chatbot_main import initial_chatbot_response
 
-# Custom CSS to centralize content
-st.markdown("""
-    <style>
-    .center {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        text-align: center;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
-# Centralize the title
-st.markdown('<div class="center"><h1>Med-Xplain</h1></div>', unsafe_allow_html=True)
+def main():
+    # Custom CSS to centralize content
+    st.markdown("""
+        <style>
+        .center {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+        }
+        </style>
+        """, unsafe_allow_html=True)
 
-# Add some space between the title and the logo
-st.markdown('<div class="center" style="margin-top: 20px;">', unsafe_allow_html=True)
-st.image("./images/MedXPlain_LOGO.png", width=200)
-st.markdown('</div>', unsafe_allow_html=True)
+    # Centralize the title
+    st.markdown('<div class="center"><h1>Med-Xplain</h1></div>', unsafe_allow_html=True)
 
-user_input = st.text_input("How can we help you today?", "")
-if st.button("Send"):
-    response = chatbot_response(user_input)
-    st.text_area("Chatbot:", response)
+    # Add some space between the title and the logo
+    st.markdown('<div class="center" style="margin-top: 20px;">', unsafe_allow_html=True)
+    st.image("./images/MedXPlain_LOGO.png", width=200)
+    st.markdown('</div>', unsafe_allow_html=True)
 
-st.write(
-    "Welcome to Med-Xplain, a patient informational tool. We aim to help you learn more about your treatment options post-diagnosis.")
+    user_input = st.text_input("How can we help you today?", "")
+    if st.button("Send"):
+        loading_text = st.empty()
+        loading_text = st.text("Generating response...")
+        # time.sleep(4)
+        response = initial_chatbot_response(user_input)
+        loading_text.empty()
+        st.text_area("Chatbot:",
+                     response, disabled=True)
+
+    st.write(
+        "Welcome to Med-Xplain, a patient informational tool. We aim to help you learn more about your treatment options post-diagnosis.")
+
+
+if __name__ == "__main__":
+    main()
--- webscraper.py
+++ webscraper.py
@@ -20,34 +20,44 @@ def extract(content: str, schema: dict):
 
 def call_nhs_search(query):
     from langchain.document_loaders import AsyncChromiumLoader
-    from langchain.document_transformers import BeautifulSoupTransformer
+    # from langchain.document_transformers import BeautifulSoupTransformer
 
+    from bs4 import BeautifulSoup
     # Load HTML
     loader = AsyncChromiumLoader(
-        [f"https://www.nhs.uk/search/results?q={'treatments for ' + urllib.parse.quote(query)})"])
+        [f"https://www.nhs.uk/search/results?q={urllib.parse.quote(query)})"])
     html = loader.load()
 
-    bs_transformer = BeautifulSoupTransformer()
+    soup = BeautifulSoup(html[0].page_content, "html.parser")
+    link_tags = soup.find_all('a')
 
-    docs_transformed = bs_transformer.transform_documents(html,
-                                                          unwanted_tags=["script", "style", "head", "title", "meta",
-                                                                         "[document]", "header"], remove_lines=True,
-                                                          tags_to_extract=["a"], attributes_to_extract=["href"])
+    # Create a list of dictionaries with title and href as key-value pairs
+    links_with_titles = [
+        {"page_title": tag.get_text().strip("\n "), "page_url": "https://www.nhs.uk/" + tag.get('href'), } for
+        tag in link_tags[11:-10]]
 
-    schema = {
-        "properties": {
-            "page_title": {"type": "string"},
-            "page_url": {"type": "string"},
-        },
-        "required": ["page_title", "page_url"],
-    }
-    extracted_content = extract(docs_transformed[0].page_content, schema=schema)
+    return links_with_titles
 
-    if len(extracted_content) > 0 and extracted_content[0]["page_title"] == "Cookies":
-        extracted_content.pop(0)
+    # bs_transformer.
+    # docs_transformed = bs_transformer.transform_documents(html,
+    #                                                       unwanted_tags=["script", "style", "head", "title", "meta",
+    #                                                                      "[document]", "header"], remove_lines=True,
+    #                                                       tags_to_extract=["a"], attributes_to_extract=["href"])
 
-    return extracted_content
+    # schema = {
+    #     "properties": {
+    #         "page_title": {"type": "string"},
+    #         "page_url": {"type": "string"},
+    #     },
+    #     "required": ["page_title", "page_url"],
+    # }
+    # extracted_content = extract(docs_transformed[0].page_content, schema=schema)
+
+    # if len(extracted_content) > 0 and extracted_content[0]["page_title"] == "Cookies":
+    #     extracted_content.pop(0)
+    # # TODO: If extracted content is empty, return a message saying that no results were found
+    # return extracted_content
 
 
 if __name__ == "__main__":
-    call_nhs_search("Lung Cancer")
+    print(call_nhs_search("lung cancer treatment options"))
