61a3e0e19958f34ad609dbd295fbbfa1922de596
Ananya Bhalla <68141051+AnanyaBhalla@users.noreply.github.com>
2023-10-21 19:44:55 +0100

    embeding_approach

---
+++ chroma_approach.py
@@ -0,0 +1,111 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+import streamlit as st
+from streamlit_chat import message
+#from utils import *
+
+from sentence_transformers import SentenceTransformer
+import pinecone
+import openai
+import streamlit as st
+openai.api_key = "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"
+#model = SentenceTransformer('all-MiniLM-L6-v2')
+cache_folder = "path/to/your/cache/directory"
+model = SentenceTransformer('all-MiniLM-L6-v2', cache_folder=cache_folder)
+
+pinecone.init(
+    api_key="0deabe30-31e5-4385-a193-bbeff060b252",  # find at app.pinecone.io
+    environment="gcp-starter"  # next to api key in console
+)
+index = pinecone.Index('chatbot')
+
+index = pinecone.Index(index_name='chatbot', project_name='Chatbot application')
+
+st.subheader("Chatbot with Langchain, ChatGPT, Pinecone, and Streamlit")
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["How can I assist you?"]
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key="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")
+
+if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+
+
+system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
+and if the answer is not contained within the text below, say 'I don't know'""")
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+
+def find_match(input):
+    print(input)
+    input_em = model.encode(input).tolist()
+    result = index.query(input_em, top_k=1, includeMetadata=True)
+    print(result)
+    return result['matches'][0]['metadata']['text']
+    #return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+def query_refiner(conversation, query):
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response['choices'][0]['text']
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+textcontainer = st.container()
+
+
+with textcontainer:
+    query = st.text_input("Query: ", key="input")
+    if query:
+        with st.spinner("typing..."):
+            conversation_string = get_conversation_string()
+            # st.code(conversation_string)
+            refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
+            context = find_match(refined_query)
+            # print(context)  
+            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response) 
+with response_container:
+    if st.session_state['responses']:
+
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i],key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
